b48893a021b36f454d528941d1aa9ee10d23a436
Non0w <arnaudcoj@protonmail.com>
2024-12-12 23:43:14 +0100

    Add Groseille button :yos:

--- change_palettes.js
+++ change_palettes.js
@@ -1,14 +1,34 @@
+const palettes = ['color', 'dmg', 'sgb'];
+
 window.onload = function() {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
-    const palettes = urlParams.get('bgps');
-    let sheet;
+    let bgps = urlParams.get('bgps');
 
-    if (palettes == undefined) {
-        sheet = 'style/palettes/color.css';
-    } else {
-        sheet = `style/palettes/${palettes}.css`;
+    if (bgps === null) {
+        bgps = palettes[0];
     }
+
+    change_palette(bgps)
+};
+
+const onGroseilleClick = function () {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    let bgps_i = palettes.indexOf(urlParams.get('bgps'));
+    if (bgps_i < 0) {
+        bgps_i = 0;
+    }
+
+    bgps_i = (bgps_i + 1) % palettes.length;
+
+    window.location.search = `bgps=${palettes[bgps_i]}`;
+};
+document.querySelectorAll(".groseille-button").forEach(b => b.addEventListener("click", onGroseilleClick));
+
+const change_palette = function (palettes) {
+    console.log(palettes);
+    const sheet = `style/palettes/${palettes}.css`;
 
     const cssId = "bgps";
     const cssIdCustom = "customBgps";
